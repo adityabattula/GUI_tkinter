305f6c1adecd0cb15819dc9c44fecc12f120a647
Aditya Battula <96254099+adityabattula@users.noreply.github.com>
2023-03-06 21:15:08 +0530

    Add files via upload

---
+++ message.py
@@ -0,0 +1,12 @@
+import tkinter as ttk
+from tkinter import Tk
+from tkinter import messagebox
+from tkinter import scrolledtext
+
+
+window = ttk.Tk()
+window.title("Hello nigga")
+txt = scrolledtext.ScrolledText(window,width=40,height=10)
+messagebox.showinfo('ERROR','FUCK OFF!')
+
+window.mainloop()
